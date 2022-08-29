42629b64c4ef1455a7dae993f5a6bfe9d620e1f4
antoine <antoine.guingand@wanadoo.fr>
2022-08-29 13:58:16 +0200

    First commit

---
+++ config.production.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'baseUrl' => getenv('BASE_URL_PRODUCTION') ?? 'https://jigsaw-blog-netlify-netlifycms-template.netlify.com',
+    'production' => true,
+];
---
+++ config.staging.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'baseUrl' => getenv('BASE_URL_STAGING') ?? 'https://jigsaw-blog-netlify-netlifycms-template.netlify.com',
+    'production' => false,
+];
